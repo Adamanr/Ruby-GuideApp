1cb032c889ac6f2b45922b9337427b0c1beb1e5a
adamanr <molchanov_03@bk.ru>
2021-09-02 21:01:47 +0500

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.0.1'
 
+gem 'bcrypt'
 gem 'rails'
 gem 'puma'
 gem 'sass-rails'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("bootstrap")
+
+// stylesheets
+require("../stylesheets/main.scss")
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
